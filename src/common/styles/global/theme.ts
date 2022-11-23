546bc8bd0c350a80a1554bcaffa99b3679861a3d
jtiagosantos <tiagosan040@gmail.com>
2022-11-22 22:01:09 -0300

    style: configure theme

---
+++ src/common/styles/global/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme, Theme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+        maxW: '1920px',
+        w: '100%',
+        mx: 'auto',
+      },
+    },
+  },
+  fonts: {
+    body: 'Poppins',
+    heading: 'Poppins',
+  },
+  colors: {},
+} as Theme | Record<string, unknown>);
