ced64497541c7a1abbc2dbb2e3915cbf3ba168ac
jtiagosantos <tiagosan040@gmail.com>
2022-11-23 23:59:47 -0300

    feat: implement stripe util function to format amount from stripe

---
+++ src/common/utils/stripe-utils/format-amount-from-stripe.util.ts
@@ -0,0 +1,19 @@
+export const formatAmountFromStripe = (amount: number, currency: string) => {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
+    style: 'currency',
+    currency: currency,
+    currencyDisplay: 'symbol',
+  });
+
+  const parts = numberFormat.formatToParts(amount);
+
+  let zeroDecimalCurrency = true;
+
+  for (const part of parts) {
+    if (part.type === 'decimal') {
+      zeroDecimalCurrency = false;
+    }
+  }
+
+  return zeroDecimalCurrency ? amount : Math.round(amount / 100);
+};
