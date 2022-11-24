ee58eaf2c597feff92c89af512e63adef7f335f0
jtiagosantos <tiagosan040@gmail.com>
2022-11-23 22:38:23 -0300

    feat: implement api endpoint to create a checkout session

---
+++ src/pages/api/checkout_sessions/index.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import Stripe from 'stripe';
+import { stripe } from '@/common/lib/stripe/stripe';
+
+import { formatAmountForStripe } from '@/common/utils/stripe-utils/format-amount-for-stripe.util';
+
+import { TCartProduct } from '@/common/types/cart-product.types';
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const { method, headers, body } = request;
+
+  if (method !== 'POST') {
+    response.setHeader('Allow', 'POST');
+    return response.status(405).end('Method Not Allowed');
+  }
+
+  try {
+    const products = body.products as Array<TCartProduct>;
+
+    const lineItems = products.map(({ product, quantity }) => ({
+      price_data: {
+        currency: 'BRL',
+        unit_amount: formatAmountForStripe(product.price, 'BRL'),
+        product_data: {
+          name: product.name,
+          description: product.description,
+          images: [product.image],
+        },
+      },
+      quantity,
+    }));
+
+    const params = {
+      mode: 'payment',
+      payment_method_types: ['card', 'boleto'],
+      line_items: lineItems,
+      success_url: `${headers.origin}?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${headers.origin}?checkout=cancel`,
+    };
+
+    const checkoutSession = await stripe.checkout.sessions.create(
+      params as Stripe.Checkout.SessionCreateParams,
+    );
+
+    response.status(200).json(checkoutSession);
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'Internal server error';
+
+    response.status(500).json({ statusCode: 500, message: errorMessage });
+  }
+}
