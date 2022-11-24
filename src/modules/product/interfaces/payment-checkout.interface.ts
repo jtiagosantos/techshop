22bcda6b7389885362334d15920ee591c53f0833
jtiagosantos <tiagosan040@gmail.com>
2022-11-23 23:59:04 -0300

    feat: restructure types in interface

--- src/modules/product/interfaces/payment-checkout.interface.ts
+++ src/modules/product/interfaces/payment-checkout.interface.ts
@@ -1,3 +1,3 @@
-import { TCartProduct } from '@/common/types/cart-product.types';
+import { TCartProduct } from '@/common/types/cart-product.type';
 
 export type IPaymentCheckout = Array<TCartProduct>;
