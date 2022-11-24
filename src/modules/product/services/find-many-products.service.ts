478390282abda807f0eec5c9f23681a5b97d7ced
jtiagosantos <tiagosan040@gmail.com>
2022-11-24 00:00:06 -0300

    feat: implement service to find many products from stripe

---
+++ src/modules/product/services/find-many-products.service.ts
@@ -0,0 +1,27 @@
+import { stripe } from '@/common/lib/stripe/stripe';
+
+import { formatAmountFromStripe } from '@/common/utils/stripe-utils/format-amount-from-stripe.util';
+
+import { TProduct } from '../types/product.type';
+
+export class FindManyProductsService {
+  public static async execute(): Promise<Array<TProduct>> {
+    const { data: productsData } = await stripe.products.list();
+    const { data: pricesData } = await stripe.prices.list();
+
+    const products = productsData.map((product, index) => {
+      const price = pricesData[index].unit_amount as number;
+      const formattedPrice = formatAmountFromStripe(price, 'BRL');
+
+      return {
+        id: product.id,
+        name: product.name,
+        description: product.description || '',
+        image: product.images[0],
+        price: formattedPrice,
+      };
+    });
+
+    return products;
+  }
+}
