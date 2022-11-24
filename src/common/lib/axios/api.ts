d5bb99888e723ab2407371786d04727709c68593
jtiagosantos <tiagosan040@gmail.com>
2022-11-23 22:44:05 -0300

    feat: get api base url from env

--- src/common/lib/axios/api.ts
+++ src/common/lib/axios/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const { API_BASE_URL } = process.env;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: API_BASE_URL,
 });
--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
       STRIPE_SECRET_KEY: string;
+      API_BASE_URL: string;
     }
   }
 }
