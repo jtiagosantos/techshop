07b5c998468576912aa56d86ad604b32c2495a20
jtiagosantos <tiagosan040@gmail.com>
2022-11-28 15:13:59 -0300

    chore: define new envionments variables

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -4,6 +4,11 @@ declare global {
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
       STRIPE_SECRET_KEY: string;
       API_BASE_URL: string;
+      STRIPE_WEBHOOK_SECRET: string;
+      NODEMAILER_HOST: string;
+      NODEMAILER_PORT: number;
+      NODEMAILER_AUTH_USER: string;
+      NODEMAILER_AUTH_PASS: string;
     }
   }
 }
